43fee02e6957323997f22724d7c39e3215c5d208
Siam-Kabir-2 <mdsiamkabir1@gmail.com>
2025-06-30 23:29:36 +0600

    Customer search implemented

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,34 @@
 import { Metadata } from "next";
 import CustomersTable from "@/app/ui/customers/table";
 import { fetchFilteredCustomers } from "@/app/lib/data";
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: 'Customers',
+  title: "Customers",
 };
 
-export default async function Page(){
-    const Customers= await fetchFilteredCustomers('');
-    return <CustomersTable customers={Customers}/>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <Suspense key={query} fallback={<div>Loading customers...</div>}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
